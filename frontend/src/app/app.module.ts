800e094dd14c99036b38ecadad20d9045f4a96cf
ziv_shmueli_segal <zivss.box@gmail.com>
2023-12-28 02:44:14 +0200

    Draft

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CalendarComponent } from './calendar/calendar.component';
+import { GetShiftsComponent } from './get-shifts/get-shifts.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CalendarComponent,
+    GetShiftsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ frontend/src/app/get-shifts.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class GetShiftsService {
+
+  constructor(private http: HttpClient) { }
+
+  getShifts(): Observable<any> {
+    return this.http.put<any>('http://localhost:8000/api/calendar/', {});
+  }
+}
